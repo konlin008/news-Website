54ae9d225b517e725f570097a187c90c2dbb89e6
Aman Mondal <162113539+konlin008@users.noreply.github.com>
2024-10-13 22:38:32 +0530

    News URL linked

--- News Website/main.js
+++ News Website/main.js
@@ -1,29 +1,52 @@
 apiKey = "5392a3988de04e7eb76be2820d0f5cf3";
 apiUrl = "https://newsapi.org/v2/everything?q=";
-newsImage = document.querySelector(".newsImg");
-newsTitle = document.querySelector(".newsTitle");
-newsDesc = document.querySelector(".newsDescription");
-newsData = document.querySelector(".newsData");
 
 window.addEventListener("load", () => fetchNews("India"));
 
 async function fetchNews(query) {
   const res = await fetch(`${apiUrl}${query}&apiKey=${apiKey}`);
   const data = await res.json();
+  console.log(data);
+
   bindDAta(data.articles);
 }
 function bindDAta(articles) {
-  var newSection = document.getElementById("news-Section");
+  var newsSection = document.getElementById("news-Section");
   var newsSectionTemp = document.getElementById("news-section-temp");
-  newSection.innerHTML = "";
+  newsSection.innerHTML = "";
 
   articles.forEach((article) => {
     if (!article.urlToImage) return;
     const cardClone = newsSectionTemp.content.cloneNode(true);
     fillDataInCard(cardClone, article);
-    newSection.appendChild(cardClone);
+    newsSection.appendChild(cardClone);
   });
-}   
-function fillDataInCard(articles) {
-  newsImage.src = articles.urlToImage;
+}
+function fillDataInCard(cardClone, article) {
+  newsImage = cardClone.querySelector("#newsImg");
+  newsTitle = cardClone.querySelector(".newsTitle");
+  newsDesc = cardClone.querySelector(".newsDescription");
+  newsData = cardClone.querySelector(".newsData");
+  newsSource = cardClone.querySelector(".source");
+  newsDate = cardClone.querySelector(".date");
+
+  const date = new Date(article.publishedAt).toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
+  });
+
+  newsImage.src = article.urlToImage;
+  newsTitle.innerHTML = article.title;
+  newsDesc.innerHTML = article.description;
+  newsSource.innerHTML = article.source.name;
+  newsDate.innerHTML = date;
+
+  cardClone.firstElementChild.addEventListener("click", () => {
+    window.open(article.url, "_blank");
+  });
+}
+
+let currentSelectedItem = null;
+
+function onNavItemClick(id) {
+  fetchNews(id);
 }
